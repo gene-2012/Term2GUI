525d3707cc3e1996e3609ec2dc9839dce45ea73e
gene-2012 <gene201248@126.com>
2024-10-26 22:49:57 +0800

    add examples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gui import Application
+from sys import argv
 if __name__ == '__main__':
-    app = Application('cfg.json')
+    app = Application(argv[1] if len(argv) > 1 else 'cfg.json')
     app.mainloop()
